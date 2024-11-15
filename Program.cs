7d7c8ff8725c09774d601371b70d8e793427e426
Davy <machadodavi3778@gmail.com>
2024-11-15 13:06:57 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Exe3
+{
+    //exercicio 3
+    public static void Main(string[] args)
+    {
+        int idade = 20;
+        bool eCasado = true;
+
+        if (idade < 18)
+        {
+            Console.WriteLine("É menor de idade");
+        }
+        else if (idade >= 18 && idade < 65)
+        {
+            if (eCasado)
+            {
+                Console.WriteLine("És casado");
+            }
+            else if(!eCasado)
+            {
+                Console.WriteLine("És solteiro");
+            }
+            Console.WriteLine("És adulto");
+        }
+
+        else
+        {
+            Console.WriteLine("Ès adulto e solteiro");
+        }
+
+
+    }
+}
